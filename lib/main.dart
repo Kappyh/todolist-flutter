871de1b23c42af8b1e6331832bffaa7f46e7c86f
Gabriela Menddes <contato@gabrielamendes.dev>
2022-08-27 23:36:11 -0300

    Adding new widget for tasks
    - Styling the list of task
    - New widget for the task
    - List view for the scroll

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController _editingController = TextEditingController();
 
+  // List<String> rowItens = List<String>.generate(50, (index) => 'Número gerado na posição $index');
+
+  List<Widget> rowItems = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,6 +56,12 @@ class _HomePageState extends State<HomePage> {
           ),
         ],*/
       ),
+      body: ListView.builder(
+        itemCount: this.rowItems.length,
+        itemBuilder: (BuildContext context, int position) {
+          return rowItems[position];
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           /*ScaffoldMessenger.of(context).showSnackBar(
@@ -69,24 +79,36 @@ class _HomePageState extends State<HomePage> {
             context: context,
             builder: (BuildContext context) {
               return AlertDialog(
-                title: Text('Título do alerta'),
-                content: Text('Conteúdo do alerta'),
+                title: Text('Nova tarefa'),
+                content: Text('Informe o título da tarefa'),
                 actions: [
-                  TextField(
-                    controller: _editingController,
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: TextField(
+                      controller: _editingController,
+                    ),
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text('Cancelar'),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      print(_editingController.text);
-                    },
-                    child: Text('Ok'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        style: TextButton.styleFrom(textStyle: TextStyle(fontWeight: FontWeight.bold,)),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Cancelar', style: TextStyle(color: Colors.red),),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          setState(() {
+                            rowItems.add(RowItem(title: _editingController.text,));
+                            _editingController.text = '';
+                          });
+                        },
+                        child: Text('Ok'),
+                      ),
+                    ],
                   ),
                 ],
               );
@@ -95,6 +117,36 @@ class _HomePageState extends State<HomePage> {
         },
         child: Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class RowItem extends StatefulWidget {
+  RowItem({Key? key, required this.title, this.isDone = false}) : super(key: key);
+
+  String title;
+  bool isDone;
+
+  @override
+  State<RowItem> createState() => _RowItemState();
+}
+
+class _RowItemState extends State<RowItem> {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.title),
+/*      subtitle: Text('Subtitle'),
+      leading: Icon(Icons.favorite),
+      trailing: Icon(Icons.done),*/
+      trailing: widget.isDone ? Icon(Icons.done): null,
+      selected: true,
+      onTap: () {
+        setState(() {
+          widget.isDone = !widget.isDone;
+        });
+      },
     );
   }
 }
